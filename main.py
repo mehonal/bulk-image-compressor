4615372c629c4c6270bd58e91e5ccb15193714ed
Mehonal <116497904+mehonal@users.noreply.github.com>
2023-04-02 06:57:55 +0300

    added clearer instructions and changed max_mb to min_mb

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ class SETTINGS:
 print('''
 	This is a script to mass compress images using jpegoptim. You must have jpegoptim installed in order to use this script.
 	Recommended settings for fast websites are as follows:
-	Max MB: 0.1 MB
+	Min MB: 0.1 MB
 	Compression: 70
 	------------------------------------------------------------
 	Recommended settings for regular users is as follows:
-	Max MB: 1 MB
+	Min MB: 1 MB
 	Compression: 75
 
 	''')
 
-max_mb = float(input('Enter max MB for images: '))
+min_mb = float(input('Enter minimum size of images to compress in MB: '))
 compression = int(input('Enter compression percentage (0-100): '))
 
 
 
 for file in os.listdir():
 	if file.endswith('JPG'):
-		if os.stat(file).st_size > (max_mb * SETTINGS.MB):
+		if os.stat(file).st_size > (min_mb * SETTINGS.MB):
 			print(f'File: {file}')
 			print("Old size: " + str(os.stat(file).st_size / SETTINGS.MB) + " MB")
 			os.system(f'jpegoptim \'{file}\' --force --max={compression}')
